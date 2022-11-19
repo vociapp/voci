54e9e09ef2e174c4d820d513a56fac92187f9150
Callum <callumbrezden@gmail.com>
2022-11-19 11:19:50 -0800

    Added Theme
    Create a theme that will act upon all MUI elements. This will make it easy to change the colours of objects.

--- src/Login/loginHeader.js
+++ src/Login/loginHeader.js
@@ -1,21 +1,48 @@
 import './Login.css';
 import {Link} from "react-router-dom";
 import React from "react";
-import { TextField } from "@mui/material"
+import { TextField } from "@mui/material";
+import { red } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from '@mui/material/styles';
+import { Button } from "@mui/material"
 
-function logoHeader() {
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7895B2'
+    },
+  },
+
+  typography: {
+    allVariants: {
+      fontFamily: 'Source Code Pro',
+      fontSize: 24,
+    },
+  },
+});
+
+export default function logoHeader() {
+
   return (
-    <div className="centerClass">
-      <div className="logoHeader">
-            <h1 className="logoText">Voci</h1>
-      </div>
+    <ThemeProvider theme={theme}>
+      <div className="centerClass">
+        <div className="logoHeader">
+              <h1 className="logoText">Voci</h1>
+        </div>
 
-      <div className="textField">
-        <TextField id="filled-basic" label="Email" variant="filled" />
-        <TextField id="filled-basic" label="Password" variant="filled" />
+        <div className="textField">
+          <TextField 
+            id="filled-basic" 
+            label="Email" 
+            variant="filled"
+            placeholder='example@gmail.com'
+          />
+          <TextField id="filled-basic" label="Password" variant="filled" />
+          <Button variant="contained">Login</Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
-
-export default logoHeader;
