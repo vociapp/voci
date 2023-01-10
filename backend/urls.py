83d9aabd86894714f3969587491ec894129d90fa
brezden <callumbrezden@gmail.com>
2023-01-09 23:32:27 -0800

    Tailwind Installed

---
+++ backend/urls.py
@@ -0,0 +1,9 @@
+from django import urls
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+urlpatterns = [
+    path('', include('frontend.urls')),
+    path('admin/', admin.site.urls),
+]
