3a020fffdaf50e8510e6b6303ef694d4b3ffbe85
brezden <callumbrezden@gmail.com>
2023-01-09 23:57:44 -0800

    Testing Secret Key

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru2#6m!+@t0=o%@n6bmsny$r593ubv+p&z0%c5j7((#54)wzzs'
+SECRET_KEY = 'u^@s3ej7qy&9xlgei=chg41!vu*z_7lco46sm+6su3z-n=(5f#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
