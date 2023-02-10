3e9d47e65327dd91b1fdcf6d2d7f6ccd645d0a7c
Jon Steele <jonwhsteele@gmail.com>
2023-02-10 15:01:42 -0800

    added new styling for smartphone users

--- cards/cards.php
+++ cards/cards.php
@@ -1,74 +1,73 @@
+            <main class="cards">
+        <!-- Display Study modes to the left -->
+                <!-- <div class="cards-menu">    
+                    <form method="post" action=".">
+                        <input type="hidden" name="action" value="study_deck">
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="hidden" name="initiate" value=1>
+                        <input type="submit" value="study">
+                    </form>
+
+                    <form>
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="submit" value="voci mode">
+                    </form>
+
+                    <form method="post" action=".">
+                        <input type="hidden" name="action" value="rename_deck_view">
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="submit" value="rename">
+                    </form>
+
+                    <form method="post" action="." onSubmit="return confirm('Are you sure you want to delete?')">
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="hidden" name="action" value="share">
+                        <input type="submit" value="share">
+                    </form>
+
+                    <form method="post" action="." onSubmit="return confirm('Are you sure you want to delete?')">
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="hidden" name="action" value="delete_deck">
+                        <input type="submit" value="delete deck">
+                    </form>
+                </div> -->
         
-        <div class="cards-grid">
-            <!-- Display Study modes to the left -->
-                    <section class="cards-side">    
-                        <!-- Study Button -->
-                        <form method="post" action=".">
-                            <input type="hidden" name="action" value="study_deck">
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="hidden" name="initiate" value=1>
-                            <input type="submit" value="study">
-                        </form>
-
-                        <!-- Voci Button  -->
-                        <form>
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="submit" value="voci mode">
-                        </form>
-                        <!-- Rename -->
-                        <form method="post" action=".">
-                            <input type="hidden" name="action" value="rename_deck_view">
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="submit" value="rename">
-                        </form>
-
-                        <!-- TODO Share -->
-                        <form method="post" action="." onSubmit="return confirm('Are you sure you want to delete?')">
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="hidden" name="action" value="delete_deck">
-                            <input type="submit" value="share">
-                        </form>
-
-                        <!-- Delete  -->
-                        <form method="post" action="." onSubmit="return confirm('Are you sure you want to delete?')">
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="hidden" name="action" value="delete_deck">
-                            <input type="submit" value="delete">
-                        </form>
-                    </section>
-           
-                    <sectio class="cards">
-<?php foreach ($cards as $card){?>
-
-                    <div class="card-and-delete">
-                        <div class="card">
-                            <p1><?php echo $card['front'];?></p1>
-                            <br>
-                            <p1><?php echo $card['back'];?></p1>
-                            <br>
-                        </div>
-                        <!-- Delete a card -->
-                        <form method="post" style="float:right;" action=".">
-                            <input type="hidden" name="action" value="delete_card">
-                            <input type = "hidden" name="card_id" value=<?php echo $card['card_id'];?>>
-                            <input class="delete" type="submit" value="Delete">
-                        </form>
-                    </div>
-<?php } ?>
                 <!--Add a card-->
-                    <div class="add-card">
-                        <form class="add-deck" method="post" action=".">
-                            <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
-                            <input type="hidden" name="action" value="add_card">
-                            <input tabindex="1" class="text" type="text" name="front" placeholder="Press tab">
-                            <br>
-                            <input tabindex="2" class="text" type="text" name = "back">
-                            <br>
-                            <input tabindex="3" class="button" type="submit" value="Add">
-                        </form>
+                <div class="add-card">
+                    <form class="add-deck" method="post" action=".">
+                        <input type="hidden" name="deck_id" value=<?php echo $_SESSION['deck_id'];?>>
+                        <input type="hidden" name="action" value="add_card">
+                        <input tabindex="1" class="text" type="text" name="front" placeholder="Press tab">
+                        <br>
+                        <input tabindex="2" class="text" type="text" name = "back">
+                        <br>
+                        <input tabindex="3" class="button" type="submit" value="Add">
+                    </form>
+                </div>
+
+                <div class="cards">
+    <?php foreach ($cards as $card){?>
+
+                <div class="card-and-delete">
+
+                    <!-- Delete a card -->
+                    <form method="post" action=".">
+                        <input type="hidden" name="action" value="delete_card">
+                        <input type = "hidden" name="card_id" value=<?php echo $card['card_id'];?>>
+                        <input class="delete" type="submit" value="Delete">
+                    </form>
+
+                    <div class="card">
+                        <p1><?php echo $card['front'];?></p1>
+                        <br>
+                        <p1><?php echo $card['back'];?></p1>
+                        <br>
                     </div>
-                </sectio>
-            </div>
+
+                </div>
+    <?php } ?>
+                </div>
+            </main>
         <!-- <footer>
             <ul>
                 <li><a href="?logout=true">log out</a></li>
--- decks/decks.php
+++ decks/decks.php
@@ -4,32 +4,34 @@ $decks = get_decks($user_id);
 ?>
         <!-- Main Section -->
         <main class="decks">
+            <div class="scrollable">
 <?php foreach ($decks as $deck){ ?>
-            <!-- <form method="post" onSubmit="return confirm('Are you sure you want to delete?')" action=".">
-                <input type="hidden" name="action" value="delete_deck">
-                <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
-                <input class="editButton" type="submit" value="Delete">
-            </form>
-            <form method="post" action=".">
-                <input type="hidden" name="action" value="edit_deck_view">
-                <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
-                <input class="editButton" type="submit" value="Edit">
-            </form> -->
-            <form class="deck" method="post" action=".">
-                <input type="hidden" name="action" value="cards_view">
-                <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
-                <input type="hidden" name="deck_name" value="<?php echo $deck['name']; ?>">
-                <input type="submit" value="<?php echo $deck['name'];?>">
-            </form>
-                <br>
+                <!-- <form method="post" onSubmit="return confirm('Are you sure you want to delete?')" action=".">
+                    <input type="hidden" name="action" value="delete_deck">
+                    <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
+                    <input class="editButton" type="submit" value="Delete">
+                </form>
+                <form method="post" action=".">
+                    <input type="hidden" name="action" value="edit_deck_view">
+                    <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
+                    <input class="editButton" type="submit" value="Edit">
+                </form> -->
+                <form class="deck" method="post" action=".">
+                    <input type="hidden" name="action" value="cards_view">
+                    <input type="hidden" name="deck_id" value=<?php echo $deck['deck_id']; ?>>
+                    <input type="hidden" name="deck_name" value="<?php echo $deck['name']; ?>">
+                    <input type="submit" value="<?php echo $deck['name'];?>">
+                </form>
+                    <br>
 <?php } ?>
-            <!-- Add Deck Div -->
-            <form action="." method="post">
-                <input type="hidden" name="action" value="add_deck">
-                <input type="hidden" name="user_id" value=<?php echo $_SESSION['user_id']; ?>>
-                <input tabindex="1" class = "add-text" type="text" name="name" placeholder="Press tab">
-                <input tabindex="2" class="add-button" type="submit" value = "Add">
-            </form>
+                <!-- Add Deck Div -->
+                <form action="." method="post">
+                    <input type="hidden" name="action" value="add_deck">
+                    <input type="hidden" name="user_id" value=<?php echo $_SESSION['user_id']; ?>>
+                    <input tabindex="1" class = "add-text" type="text" name="name" placeholder="Press tab">
+                    <input tabindex="2" class="add-button" type="submit" value = "Add">
+                </form>
+            </div>
         </main>
         <br>
         <!-- Footer Section -->
