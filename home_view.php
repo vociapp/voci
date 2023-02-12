cb06fae85ecfa24adaf62bd8edc3dd985314f52f
Jon Steele <jonwhsteele@gmail.com>
2023-02-11 17:11:34 -0800

    Updated home and home styles

--- home_view.php
+++ home_view.php
@@ -1,5 +1,15 @@
 <main class="home">
     <h>welcome to voci.</h>
+    <br>
+    <p.home>(voice operated card interface)</p.home>
+    <br><br><br>
+    <p.home>
+        If you are new here, select "decks" to create a new account. 
+    </p.home>
+    <p.home>
+        From there, log in to create and access your decks.
+    </p.home>
+    <br>
 </main>
 </body>
 
