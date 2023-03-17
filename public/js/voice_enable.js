ebb1db744f57586c6e1e47f4f01dc47b99bc1e3a
Jon Steele <jonwhsteele@gmail.com>
2023-03-17 14:40:54 -0700

    Removed need for button

--- public/js/voice_enable.js
+++ public/js/voice_enable.js
@@ -1,16 +1,18 @@
+let firstInteraction = false;
+
 function speak(text) {
     const msg = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(msg);
 }
 
 function onFirstInteraction() {
-    document.removeEventListener('touchstart', onFirstInteraction);
-    const hiddenButton = document.getElementById('hiddenButton');
-    hiddenButton.click();
+    if (!firstInteraction) {
+        firstInteraction = true;
+        document.removeEventListener('touchstart', onFirstInteraction);
+        speak('Hello, this is your speech synthesis on page reload!');
+    }
 }
 
 window.onload = () => {
-    const hiddenButton = document.getElementById('hiddenButton');
-    hiddenButton.addEventListener('click', () => speak('Hello, this is your speech synthesis on page reload!'));
     document.addEventListener('touchstart', onFirstInteraction);
 };
