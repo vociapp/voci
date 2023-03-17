d0ff6cfbe28a7eca843141c7e246709eca9f7ebd
Jon Steele <jonwhsteele@gmail.com>
2023-03-17 14:11:54 -0700

    Refactored welcome function

--- resources/views/study/prepare.blade.php
+++ resources/views/study/prepare.blade.php
@@ -17,16 +17,13 @@
         button.setAttribute("onclick", "welcome()");
 
         function welcome(){
-            // Acquiring parameters to speak from php
-            let read = document.getElementById("read").textContent;
-            let rate = document.getElementById("rate").textContent;
-            let voice_style = document.getElementById("voices_dropdown").textContent;
+            let read = 'Welcome';
+            let rate = 1;
 
             speechSynthesis.cancel();
             // Setting up the utterance as a global variable, so the controller scripts can access it 
             utterance = new SpeechSynthesisUtterance(read);
             utterance.rate = rate;
-            utterance.voice = speechSynthesis.getVoices().find(v => v.name === voice_style);
 
             // Activating the utterance
             speechSynthesis.speak(utterance);
