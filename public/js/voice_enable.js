4aafd1a643bf23a0bd25784fa489960ccd29916c
Jon Steele <jonwhsteele@gmail.com>
2023-03-17 14:37:53 -0700

    Changed nature of voice enable

--- public/js/voice_enable.js
+++ public/js/voice_enable.js
@@ -1,49 +1,16 @@
-const button = document.getElementById("voiceEnable");
-// button.setAttribute("onclick", "welcome()");
-
-// Necessary for safari
-// Safari will only speak after speaking from a button click
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-function SpeakText() {
-    var msg = new SpeechSynthesisUtterance();
+function speak(text) {
+    const msg = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(msg);
-
-    document.getElementsByClassName("wc-mic")[0].removeEventListener("click", SpeakText);
 }
 
-if (isSafari) {
-
-    window.addEventListener("load", function () {
-        document.getElementsByClassName("wc-mic")[0].addEventListener("click", SpeakText);
-    });
+function onFirstInteraction() {
+    document.removeEventListener('touchstart', onFirstInteraction);
+    const hiddenButton = document.getElementById('hiddenButton');
+    hiddenButton.click();
 }
 
-// Needed to change between the two audio contexts
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var context;
-var processor;
-
-// Overrides the base constructor to use a singleton like structure
-// Needed for Safari
-var BasePrototype = AudioContext.prototype;
-AudioContext = function () {
-    return context;
-};
-AudioContext.prototype = BasePrototype;
-
-// Sets the old style getUserMedia to use the new style that is supported in more browsers
-window.navigator.getUserMedia = function (constraints, successCallback, errorCallback) {
-    context = new BasePrototype.constructor;
-    processor = context.createScriptProcessor(1024, 1, 1);
-    processor.connect(context.destination);
-
-    window.navigator.mediaDevices.getUserMedia(constraints)
-        .then(function (e) {
-            successCallback(e);
-        })
-        .catch(function (e) {
-            errorCallback(e);
-        });
+window.onload = () => {
+    const hiddenButton = document.getElementById('hiddenButton');
+    hiddenButton.addEventListener('click', () => speak('Hello, this is your speech synthesis on page reload!'));
+    document.addEventListener('touchstart', onFirstInteraction);
 };
