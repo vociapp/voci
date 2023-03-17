658053caf595835dc2144730f5c2cdbbe008456c
Jon Steele <jonwhsteele@gmail.com>
2023-03-17 14:43:38 -0700

    Added need for overlay to voice enable script

--- public/js/voice_enable.js
+++ public/js/voice_enable.js
@@ -1,18 +1,18 @@
 let firstInteraction = false;
 
 function speak(text) {
-    const msg = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(msg);
+  const msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
 }
 
 function onFirstInteraction() {
-    if (!firstInteraction) {
-        firstInteraction = true;
-        document.removeEventListener('touchstart', onFirstInteraction);
-        speak('Hello, this is your speech synthesis on page reload!');
-    }
+  if (!firstInteraction) {
+    firstInteraction = true;
+    document.removeEventListener('touchstart', onFirstInteraction);
+    speak('Hello, this is your speech synthesis on page reload!');
+  }
 }
 
 window.onload = () => {
-    document.addEventListener('touchstart', onFirstInteraction);
+  document.addEventListener('touchstart', onFirstInteraction);
 };
