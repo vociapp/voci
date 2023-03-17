ece48c7f4087ce1969e1870b5904fd2b98393a9b
Jon Steele <jonwhsteele@gmail.com>
2023-03-16 22:46:09 -0700

    Added interim detection

--- resources/views/study/show.blade.php
+++ resources/views/study/show.blade.php
@@ -20,7 +20,7 @@
         {{-- ****************************************** --}}
 
         @if (session('voice') == "true")
-            <p><a href="{{ route('study.show', $deck) }}">next </a> | repeat | <a href="{{ route('decks.index') }}"> exit</a></p>
+            <p><a href="{{ route('study.show', $deck) }}">continue </a> | repeat | <a href="{{ route('decks.index') }}"> exit</a></p>
             {{-- Passing voice rate from php into JS --}}
             <div id="rate" style="display: none">{{ session('rate'); }}</div>
             <div id="voice_style" style="display: none">{{ session('voice_style'); }}</div>
