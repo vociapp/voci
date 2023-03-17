7aa1640c94ecca8871895a030ee24a02bf355a74
Jon Steele <jonwhsteele@gmail.com>
2023-03-17 14:34:21 -0700

    Added voice enable script for ios

--- resources/views/study/prepare.blade.php
+++ resources/views/study/prepare.blade.php
@@ -12,56 +12,6 @@
             voices_dropdown.innerHTML = voices.filter(voice => voice.lang.includes('en')).map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`).join('');
         }, 50);
 
-        const button = document.getElementById("voiceEnable");
-        // button.setAttribute("onclick", "welcome()");
-
-        // Necessary for safari
-        // Safari will only speak after speaking from a button click
-        var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-        function SpeakText() {
-            var msg = new SpeechSynthesisUtterance();
-            window.speechSynthesis.speak(msg);
-
-            document.getElementsByClassName("wc-mic")[0].removeEventListener("click", SpeakText);
-        }
-
-        if (isSafari) {
-
-            window.addEventListener("load", function () {
-                document.getElementsByClassName("wc-mic")[0].addEventListener("click", SpeakText);
-            });
-        }
-
-        // Needed to change between the two audio contexts
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
-
-        var context;
-        var processor;
-
-        // Overrides the base constructor to use a singleton like structure
-        // Needed for Safari
-        var BasePrototype = AudioContext.prototype;
-        AudioContext = function () {
-            return context;
-        };
-        AudioContext.prototype = BasePrototype;
-
-        // Sets the old style getUserMedia to use the new style that is supported in more browsers
-        window.navigator.getUserMedia = function (constraints, successCallback, errorCallback) {
-            context = new BasePrototype.constructor;
-            processor = context.createScriptProcessor(1024, 1, 1);
-            processor.connect(context.destination);
-
-            window.navigator.mediaDevices.getUserMedia(constraints)
-                .then(function (e) {
-                    successCallback(e);
-                })
-                .catch(function (e) {
-                    errorCallback(e);
-                });
-        };
-
         // function welcome(){
         //     let read = 'Welcome';
         //     let rate = 1;
