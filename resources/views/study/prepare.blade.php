95dc61d287ad90245790e521bb550ad72ecd4445
Jon Steele <jonwhsteele@gmail.com>
2023-06-02 17:13:28 -0700

    Removes testing button

--- resources/views/study/prepare.blade.php
+++ resources/views/study/prepare.blade.php
@@ -27,8 +27,6 @@
 
     </script>
 
-    <button onclick="welcome()">Enable speech</button>
-
     {{-- Setup Form --}}
     <form class="w-full h-full md:w-2/3 xl:w-1/3 flex flex-col justify-center items-center rounded-md shadow-lg p-8 m-16 outline outline-1 outline-white" method="get" action="{{ route('study.initialize', $deck) }}">
         <div class="flex justify-center items-center">
